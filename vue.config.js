36b7b78c634ad60b169a6983350732a340bd27d8
Vexth <oo932758@sina.com>
2018-11-16 11:01:52 +0800

    增加备注

--- vue.config.js
+++ vue.config.js
@@ -10,10 +10,10 @@ module.exports = {
     proxy: {
       '/': {
         ws: false,
-        target: 'https://appdev.yqsqpt.com/',//设置你调用的接口域名和端口号 别忘了加http
+        target: 'https://xxx.com/',// 设置你调用的接口域名和端口号 别忘了加http, 比如：http://40.00.100.100:3002/
         changeOrigin: true,
         pathRewrite: {
-          '^/': ''//这里理解成用‘/’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'http://40.00.100.100:3002/user/add'，直接写‘/user/add’即可
+          '^/': ''// 这里理解成用‘/’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'http://40.00.100.100:3002/user/add'，直接写‘/user/add’即可
         }
       },
     }
