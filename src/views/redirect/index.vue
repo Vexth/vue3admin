075e5bc332c24083d6864de94be5dba855ce1728
Vexth <oo932758@sina.com>
2018-11-16 10:50:44 +0800

    增加刷新中间件

---
+++ src/views/redirect/index.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { Component, Vue, Watch } from 'vue-property-decorator';
+@Component
+export default class Redirect extends Vue {
+  beforeCreate() {
+    const { params, query } = this.$route;
+    const { path } = params;
+    const url = path === undefined ? '/' : ('/' + path);
+
+    this.$router.replace({ path: url, query });
+  }
+  render(h: any) {
+    return h(); // avoid warning message
+  }
+}
+</script>
